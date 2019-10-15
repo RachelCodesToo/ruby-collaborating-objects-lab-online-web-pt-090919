899b454afa4a8d3b97e24c99143330fa0c3c3330
Anne Fausto <a.r.fausto@gmail.com>
2019-10-15 16:31:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,8 +16,8 @@ class Song
   def self.new_by_filename(filename)
     artist, song = filename.split(' - ')
     new_song = new(song)
-    new_song.artist_name = 
-  
+    new_song.artist_name = artist
+    
 
 
 end 
